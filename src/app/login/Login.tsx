238f5924a73bd653365d693d860f293c17e4ff3b
achodor <arek@chodor.dev>
2020-11-16 21:49:47 +0100

    Feature: Login page

--- src/app/login/Login.tsx
+++ src/app/login/Login.tsx
@@ -1,28 +1,50 @@
+import Button from 'components/Button/Button';
+import Input from 'components/Input/Input';
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-import { AppRoute } from 'routing/AppRoute.enum';
+import styles from './login.module.css';
+import logo from 'assets/logo.svg';
+import loginBg from 'assets/login_bg.png';
 
 export const Login = () => {
   return (
-    <>
-      <Link to={AppRoute.home}>Products page</Link>
-      <h2>Login</h2>
-      <form>
-        <div>
-          <label>
-            username:
-            <input name="username" />
-          </label>
+    <div className={styles.loginWrapper}>
+      <div
+        className={styles.loginBackground}
+        style={{ backgroundImage: `url(${loginBg})` }}
+      ></div>
+      <div className={styles.login}>
+        <div className={styles.loginLogo}>
+          <img src={logo} alt="join.tsh.io" />
         </div>
-        <div>
-          <label>
-            password:
-            <input name="password" type="password" />
-          </label>
+        <div className={styles.loginFormWrapper}>
+          <h2 className={styles.loginHeader}>Login</h2>
+          <form>
+            <div className={styles.loginElement}>
+              <Input
+                className={styles.loginFormElement}
+                type="text"
+                placeholder="Username"
+                label="Username"
+              />
+            </div>
+            <div className={styles.loginElement}>
+              <Input
+                className={styles.loginFormElement}
+                type="password"
+                placeholder="Password"
+                label="Password"
+              />
+            </div>
+            <Button className={styles.loginFormButton} type="submit">
+              Log in
+            </Button>
+            <a href="#" className={styles.loginForgotPassword}>
+              Forgot password?
+            </a>
+          </form>
         </div>
-        <button type="submit">submit</button>
-      </form>
-    </>
+      </div>
+    </div>
   );
 };
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -4,12 +4,13 @@ import clsx from 'clsx';
 import styles from './input.module.css';
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
   icon?: string;
   iconAlt?: string;
   className?: string;
 }
 
-const Input = ({ icon, iconAlt, className, ...props }: InputProps) => {
+const Input = ({ label, icon, iconAlt, className, ...props }: InputProps) => {
   return (
     <>
       {icon ? (
@@ -17,6 +18,11 @@ const Input = ({ icon, iconAlt, className, ...props }: InputProps) => {
           <input {...props} className={styles.input} />
           <img className={styles.inputIcon} src={icon} alt={iconAlt} />
         </div>
+      ) : label ? (
+        <label className={styles.inputLabel}>
+          {label}
+          <input {...props} className={clsx(styles.input, className)} />
+        </label>
       ) : (
         <input {...props} className={clsx(styles.input, className)} />
       )}
